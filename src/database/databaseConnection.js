e9c55421d259cb11827a778f3501f5256074d57c
Jonathan Wesnaes <jowes18@student.sdu.dk>
2022-09-20 11:27:22 +0200

    clean up

--- src/database/databaseConnection.js
+++ src/database/databaseConnection.js
@@ -3,7 +3,7 @@ require("dotenv/config");
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.USER_NAME1}:${process.env.USER_PASS1}@cluster0.df1do.mongodb.net/RPC-Database?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.df1do.mongodb.net/RPC-Database?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .catch((err) => {
